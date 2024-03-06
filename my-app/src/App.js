473ed8391a1137d3248a8ce681ba839429af9d46
MihirPatel <60762325+TestingGround00@users.noreply.github.com>
2024-03-05 19:34:42 -0500

    added my name into the app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello World! My name is Mihir :)
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am going to refer to this site to learn React.
         </a>
       </header>
     </div>
